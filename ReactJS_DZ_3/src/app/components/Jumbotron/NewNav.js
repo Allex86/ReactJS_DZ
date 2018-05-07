4508436c3b2d3f3579cda1a8a5a87160efc2eb60
Allex86 <38761034+Allex86@users.noreply.github.com>
2018-05-08 04:43:51 +1000

    dz_3

--- ReactJS_DZ_3/src/app/components/Jumbotron/NewNav.js
+++ ReactJS_DZ_3/src/app/components/Jumbotron/NewNav.js
@@ -15,9 +15,9 @@ export default class NewNav extends Component {
                                   <li><a href="#">Action</a></li>
                                   <li><a href="#">Another action</a></li>
                                   <li><a href="#">Something else here</a></li>
-                                  <li className="divider"></li>
+                                  <li className="divider"/li>
                                   <li><a href="#">Separated link</a></li>
-                                  <li className="divider"></li>
+                                  <li className="divider"/li>
                                   <li><a href="#">One more separated link</a></li>
                               </ul>
                           </li>
